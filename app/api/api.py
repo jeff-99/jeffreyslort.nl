47c96f5f2923593b7f7934ee03de40bbd08aeb3f
jeff-99 <j_slort@hotmail.com>
2014-10-29 15:47:57 +0100

    ajax overwrite terug gedraaid

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,5 @@
 __author__ = 'Jeffrey Slort'
-from flask import Blueprint, jsonify, redirect, url_for, render_template
+from flask import Blueprint, jsonify, redirect, url_for, render_template,session
 from app import db
 
 apiBP = Blueprint("api",__name__,template_folder="templates")
@@ -11,4 +11,20 @@ def index():
 @apiBP.route("/<string:sectie>")
 def sectie(sectie):
     doc = db.secties.find_one({"sectie":sectie},{"_id":0})
-    return render_template(doc["template"],properties=doc["properties"])
+    if doc:
+        if doc.get("type") == "overzicht":
+            posts = [p for p in db.posts.find({"sectie":sectie})]
+            return render_template(doc["template"],posts=posts)
+        else:
+            return render_template(doc["template"])
+    return "er ging iets mis"
+
+@apiBP.route("/test")
+def test():
+    user = 4
+    print session.keys()
+    if not session.has_key("user_id"):
+        session["user_id"] = user
+        return "welkom user 4"
+    else:
+        return "je bent ingelogd {}".format(session["user_id"])
